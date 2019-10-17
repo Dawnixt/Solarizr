e90273d48ca3285e849b059a02ff340239921daf
Pablo <36712068+PabloT90@users.noreply.github.com>
2019-10-17 21:02:57 +0200

    login
    login terminado

--- Solarizr/Solarizr/MainPage.xaml.cs
+++ Solarizr/Solarizr/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,10 @@ namespace Solarizr
         public MainPage()
         {
             this.InitializeComponent();
+            ApplicationView.PreferredLaunchViewSize = new Size(600, 700);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(600, 700));
+
         }
     }
 }
